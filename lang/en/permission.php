80a589d34298aff553cb0ece2ce5692256f127df
Yugo <dedy.yugo.purwanto@gmail.com>
2026-08-31 11:46:01 +0700

    Register permission and trans (#4)

---
+++ lang/en/permission.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'name_browse_translation' => 'Browse translations',
+    'name_read_translation' => 'Read translation',
+    'name_edit_translation' => 'Edit translation',
+    'name_add_translation' => 'Add translation',
+    'name_delete_translation' => 'Delete translation',
+    'name_import_translation' => 'Import translations',
+    'name_export_translation' => 'Export translations',
+    'name_backup_translation' => 'Backup translations',
+    'name_restore_translation' => 'Restore translations',
+];
